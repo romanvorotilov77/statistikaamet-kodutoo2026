0ec170b106f805aa46ac8db700226d93f7f3d04a
Your Name <rovoro@taltech.ee>
2026-03-21 20:08:08 +0200

    refactor: fix logo positioning

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,13 +3,13 @@ import logo from '../assets/images/ES_Logo.png';
 
 export function Banner() {
 
-  const wrapperClasses = 'w-full h-[100px] bg-white bg-dots px-8 md:px-16 flex items-center';
-  const contentClasses = 'max-w-400 mx-auto w-full flex justify-start items-center';
+  const wrapperClasses = 'w-full h-[100px] bg-white bg-dots pl-9.5 flex items-center';
+  const contentClasses = 'flex items-center';
 
   return (
     <div className={wrapperClasses}>
       <div className={contentClasses}>
-        <div className="bg-white p-4 inline -ml-7.25 mt-2">
+        <div className="bg-white p-4 inline mt-2">
           <img 
             src={logo} 
             alt="Eesti Statistika" 
